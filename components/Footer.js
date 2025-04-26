5748486f1a597bb983b003d34f9fbe1ac71eeaa1
MukulTiwari02 <mukultiwari2020@gmail.com>
2025-04-26 16:40:23 +0530

    Footer logo fix

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="mt-auto text-gray-200 text-sm text-center w-full border-t border-neon-cyan py-2">
       <p className="flex gap-3 justify-center">
-        &copy; {new Date().getFullYear()} PlayPico - Mini Games <img src="logo.png" className="h-5" alt="logo" />  Developed with
+        &copy; {new Date().getFullYear()} PlayPico - Mini Games <img src="/logo.png" className="h-5" alt="logo" />  Developed with
         ❤️ By{" "}
         <a className="underline" href="https://mukultiwari02.github.io/Portfolio.github.io/">
           Mukul Tiwari
