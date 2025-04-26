de9b74d1f8fae4e9eb09125698c21069a212432f
MukulTiwari02 <mukultiwari2020@gmail.com>
2025-04-26 14:33:55 +0530

    Added touch device compatibility in snake game

--- pages/games/snake.js
+++ pages/games/snake.js
@@ -3,6 +3,15 @@ import Snake from "@/helpers/snake";
 import GameLayout from "@/components/GameLayout";
 
 const SnakeGame = () => {
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+
+    // Set initial width
+    handleResize();
+  }, []);
+
   const gridSize = 30;
   const initialSnakePosition = [
     [5, 5],
@@ -22,7 +31,6 @@ const SnakeGame = () => {
 
   const isSnakeDirectionEmpty = !snake.direction || snake.direction === "";
 
- 
   const generateFood = () => {
     let newFood;
     do {
@@ -51,16 +59,14 @@ const SnakeGame = () => {
         const snakeCopy = new Snake([...snake.body], food, gridSize);
         snakeCopy.setDirection(snake.direction);
 
-       
         if (snakeCopy.move()) {
           const newFood = generateFood();
-          setFood(newFood); 
+          setFood(newFood);
           snakeCopy.food = newFood;
           snakeCopy.grow();
           setScore(score + 1);
         }
 
-        
         if (snakeCopy.hasCollided()) {
           setGameOver(true);
           setGameRunning(false);
@@ -73,60 +79,69 @@ const SnakeGame = () => {
     }
   }, [gameRunning, snake, food, gameOver]);
 
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      switch (e.key) {
-        case "ArrowUp":
-          setSnake((prevSnake) => {
-            prevSnake.setDirection("UP");
-            return prevSnake;
-          });
-          break;
-        case "ArrowDown":
-          setSnake((prevSnake) => {
-            prevSnake.setDirection("DOWN");
-            return prevSnake;
-          });
-          break;
-        case "ArrowLeft":
-          setSnake((prevSnake) => {
-            prevSnake.setDirection("LEFT");
-            return prevSnake;
-          });
-          break;
-        case "ArrowRight":
-          setSnake((prevSnake) => {
-            prevSnake.setDirection("RIGHT");
-            return prevSnake;
-          });
-          break;
-        case " ":
-          if (gameOver) {
-            restartGame();
-          } else {
-            setGameRunning(!gameRunning);
-            if (!snake.direction) {
-              setSnake((currSnake) => {
-                currSnake.setDirection("RIGHT");
-                return currSnake;
-              });
-            }
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    switch (e.key) {
+      case "ArrowUp":
+        setSnake((prevSnake) => {
+          prevSnake.setDirection("UP");
+          return prevSnake;
+        });
+        break;
+      case "ArrowDown":
+        setSnake((prevSnake) => {
+          prevSnake.setDirection("DOWN");
+          return prevSnake;
+        });
+        break;
+      case "ArrowLeft":
+        setSnake((prevSnake) => {
+          prevSnake.setDirection("LEFT");
+          return prevSnake;
+        });
+        break;
+      case "ArrowRight":
+        setSnake((prevSnake) => {
+          prevSnake.setDirection("RIGHT");
+          return prevSnake;
+        });
+        break;
+      case " ":
+        if (gameOver) {
+          restartGame();
+        } else {
+          setGameRunning(!gameRunning);
+          if (!snake.direction) {
+            setSnake((currSnake) => {
+              currSnake.setDirection("RIGHT");
+              return currSnake;
+            });
           }
-          break;
-        default:
-          break;
-      }
-    };
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
+  const handleTap = (e, key) => {
+    if (windowWidth >= 1024) return;
+    e.key = key;
+    handleKeyDown(e);
+  };
+
+  useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [gameOver, gameRunning]);
 
   let instructionMessage = "";
   if (gameOver) {
-    instructionMessage = "Press Space to Restart";
+    instructionMessage =
+      windowWidth < 1024 ? "Tap to Restart" : "Press Space to Restart";
   } else if (!gameRunning) {
-    instructionMessage = "Press Space to Continue";
+    instructionMessage =
+      windowWidth < 1024 ? "Tap to Continue" : "Press Space to Continue";
   }
 
   function setGameSpeed(ev) {
@@ -150,6 +165,7 @@ const SnakeGame = () => {
       </div>
 
       <div
+        onClick={(e) => handleTap(e, " ")}
         className="relative flex overflow-hidden justify-center items-center border-4 border-black bg-violet-800"
         style={{
           width: `${gridSize * 2}vmin`,
@@ -185,13 +201,92 @@ const SnakeGame = () => {
           </div>
         )}
         {instructionMessage && (
-          <div className="absolute text-gray-200 text-2xl font-bold bottom-4">
+          <div className="absolute text-gray-200 text-sm sm:text-2xl font-bold bottom-4">
             {instructionMessage}
           </div>
         )}
       </div>
+      <Controls handleTap={handleTap}/>
     </GameLayout>
   );
 };
+
+function Controls({ handleTap }) {
+  function handler(e,key) {
+    handleTap(e, key);
+  }
+
+  return (
+    <div className="hidden max-lg:flex flex-col w-full mt-5 justify-center items-center">
+      <span className="text-xl mb-2">Controls</span>
+      <button onClick={(e) => handler(e, "ArrowUp")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-9"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+      <div className="flex gap-10">
+        <button onClick={(e) => handler(e, "ArrowLeft")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-9"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+        <button onClick={(e) => handler(e, "ArrowRight")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-9"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+      </div>
+      <button onClick={(e) => handler(e, "ArrowDown")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-9"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
 
 export default SnakeGame;
